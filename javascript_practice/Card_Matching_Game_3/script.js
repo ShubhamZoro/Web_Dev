605a8cc3f564c6317b3995a2b81105a32a3291fc
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2024-08-28 00:51:04 +0530

    Add files via upload

---
+++ javascript_practice/Card_Matching_Game_3/script.js
@@ -0,0 +1,118 @@
+var errors = 0;
+var cardList = [
+    "darkness",
+    "double",
+    "fairy",
+    "fighting",
+    "fire",
+    "grass",
+    "lightning",
+    "metal",
+    "psychic",
+    "water"
+]
+
+var cardSet;
+var board = [];
+var rows = 4;
+var columns = 5;
+
+var card1Selected;
+var card2Selected;
+
+window.onload = function() {
+    document.getElementById("start").addEventListener("click", startGame);
+    document.getElementById("reset").addEventListener("click", resetGame);
+}
+
+function shuffleCards() {
+    cardSet = cardList.concat(cardList);
+    console.log(cardSet);
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length);
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
+}
+
+function startGame() {
+    shuffleCards();
+    document.getElementById("start").classList.add("hide");
+    document.getElementById("reset").classList.remove("hide");
+    document.querySelector("h2").classList.remove("hide");
+    document.getElementById("board").classList.remove("hide");
+    
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg);
+
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = "back.jpg";
+            card.classList.add("card");
+            card.addEventListener("click", selectCard);
+            document.getElementById("board").append(card);
+        }
+        board.push(row);
+    }
+    console.log(board);
+    setTimeout(hideCards, 1000);
+}
+
+function hideCards() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "back.jpg";
+        }
+    }
+}
+
+function selectCard() {
+    if (this.src.includes("back")) {
+        if (!card1Selected) {
+            card1Selected = this;
+
+            let coords = card1Selected.id.split("-");
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = board[r][c] + ".jpg";
+        } else if (!card2Selected && this != card1Selected) {
+            card2Selected = this;
+
+            let coords = card2Selected.id.split("-");
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = board[r][c] + ".jpg";
+            setTimeout(update, 1000);
+        }
+    }
+}
+
+function update() {
+    if (card1Selected.src !== card2Selected.src) {
+        card1Selected.src = "back.jpg";
+        card2Selected.src = "back.jpg";
+        errors += 1;
+        document.getElementById("errors").innerText = errors;
+    }
+
+    card1Selected = null;
+    card2Selected = null;
+}
+
+function resetGame() {
+    document.getElementById("start").classList.remove("hide");
+    document.getElementById("reset").classList.add("hide");
+    document.querySelector("h2").classList.add("hide");
+    document.getElementById("board").classList.add("hide");
+    errors = 0;
+    document.getElementById("errors").innerText = errors;
+    document.getElementById("board").innerHTML = ''; // Clear the board
+}
